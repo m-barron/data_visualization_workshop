3ac98784f6a60930b89b3833c442ff5623e1bd5c
m-barron <33097787+m-barron@users.noreply.github.com>
2017-10-26 10:16:06 -0400

    Add files via upload

--- data_viz_script.r
+++ data_viz_script.r
@@ -7,53 +7,84 @@
 #
 #
 
-library("ggplot2")
-data("midwest", package="ggplot2")
+# install.packages("ggplot2")
+# load package and data
+options(scipen=999)  # turn-off scientific notation like 1e+48
+library(ggplot2)
+theme_set(theme_bw())  # pre-set the bw theme.
+data("midwest", package = "ggplot2")
 
-dat <- midwest
+# Scatterplot
+gg <- ggplot(midwest, aes(x=area, y=poptotal)) + 
+  geom_point(aes(col=state, size=popdensity)) + 
+  geom_smooth(method="loess", se=F) + 
+  xlim(c(0, 0.1)) + 
+  ylim(c(0, 500000)) + 
+  labs(subtitle="Area Vs Population", 
+       y="Population", 
+       x="Area", 
+       title="Scatterplot", 
+       caption = "Source: midwest") +
+  theme_bw() +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) 
+ 
 
+plot(gg)
 
-## Histogram
-
-ggplot(midwest, aes(x = percollege)) +
-  geom_histogram()
-
-
-## Scatter Plot
-
-# Relationship between poverty and college education
-
-ggplot(midwest,
-       aes(x=percbelowpoverty,
-           y=percollege,
-           color=state))+
-  geom_point()
-
-
-
-
-
-
+ggsave(gg, file="goal_plot.tiff")
 
 
+### Exercises 1 - 
+
+# 1
+ggplot(midwest, aes(x=percollege, y=percbelowpoverty)) + 
+  geom_point() # Use geom point to add points.
+
+# 2
+ggplot(midwest, aes(x=percollege, y=percbelowpoverty)) + 
+  geom_point(color="red") # Use geom point to add points.
+
+# 3
+
+ggplot(midwest, aes(x=percollege, y=percbelowpoverty)) + 
+  geom_point(aes(color = popdensity)) # Use geom point to add points.
+
+# 4
+
+ggplot(midwest, aes(x=percollege, y=percbelowpoverty)) + 
+  geom_point(aes(color = popdensity), size=3) # Use geom point to add points.
+
+# 5 
+
+ggplot(midwest, aes(x=percollege, y=percbelowpoverty)) + 
+  geom_point(aes(color = popdensity, size=poptotal)) # Use geom point to add points.
+
+# 6 
+
+ggplot(midwest, aes(x=percollege, y=percbelowpoverty)) + 
+  geom_point(aes(color = popdensity, size=poptotal)) + # Use geom point to add points.
+  geom_smooth()
 
 
+### Exercises 2 
 
+# 1 
+ggplot(midwest, aes(x=percollege, y=percbelowpoverty)) + 
+  geom_point() # Use geom point to add points.
 
+# 2
+ggplot(midwest, aes(x=percollege, y=percbelowpoverty)) + 
+  geom_point() + # Use geom point to add points.
+  geom_smooth(method='glm')
 
+# 3
+ggplot(midwest, aes(x=percollege, y=percbelowpoverty)) + 
+  geom_point() + # Use geom point to add points.
+  geom_line(stat = "smooth", method='lm')
 
-
-
-
-
-
-
-
-
-
-
-
-
+# 4 
+ggplot(midwest, aes(x=as.factor(inmetro), fill=state)) + 
+  geom_bar(position="dodge")
 
 
 
